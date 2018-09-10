c8b27fd68134c554c76687dc98efc613eab6b17b
Tinashe <tashtin263@gmail.com>
2018-09-09 23:27:45 -0700

    add SubmissionService

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 [![Build Status](https://travis-ci.org/musonza/laravel-forms.svg?branch=master)](https://travis-ci.org/musonza/laravel-forms)
+
 WIP
--- src/Form.php
+++ src/Form.php
@@ -5,6 +5,7 @@ namespace Musonza\Form;
 use Musonza\Form\Services\AnswerService;
 use Musonza\Form\Services\FormService;
 use Musonza\Form\Services\QuestionService;
+use Musonza\Form\Services\SubmissionService;
 
 class Form
 {
@@ -13,10 +14,12 @@ class Form
     public function __construct(
         FormService $formService,
         QuestionService $questionService,
-        AnswerService $answerService) {
+        AnswerService $answerService,
+        SubmissionService $submissionService) {
         $this->formService = $formService;
         $this->questionService = $questionService;
         $this->answerService = $answerService;
+        $this->submissionService = $submissionService;
     }
 
     public function create(array $data)
@@ -42,5 +45,10 @@ class Form
     public function answerService()
     {
         return $this->answerService;
+    }
+
+    public function submissionService()
+    {
+        return $this->submissionService;
     }
 }
